eecb48df8a3213e0a4884a14a92a5274d6137a73
Anadi <anirbanmondal53717@gmail.com>
2026-09-24 22:48:38 +0530

    added ATTENDENCE DAO

---
+++ src/main/java/DAO/AttendenceDAO.java
@@ -0,0 +1,25 @@
+package DAO;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import Model.Attendence;
+
+public class AttendenceDAO {
+	
+	//taking Student Attendence
+    public void takeAttendence(List<Attendence> attendenceList) {
+       	
+    }
+    
+    //calculating percentage of attendence of a batch
+    public double batchAttendence(int id) {
+    	
+    }
+    
+    
+    //calculating attendence of a student
+    public double studentAttendence(int id) {
+    	
+    }
+}
